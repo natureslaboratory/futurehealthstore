f710c246576dcf36375eea873939875d40e44b8a
Caleb Wilson <caleb@natureslaboratory.co.uk>
2021-11-05 15:25:40 +0000

    removed more ha redundant functions

--- wp-content/themes/herbalapothecary-child__futurehealthstore/functions.php
+++ wp-content/themes/herbalapothecary-child__futurehealthstore/functions.php
@@ -7,8 +7,9 @@ function fh_enqueue_assets()
 add_action('wp_enqueue_scripts', 'fh_enqueue_assets');
 
 
-add_action('init', 'remove_my_custom_search');
-function remove_my_custom_search()
+add_action('init', 'remove_ha_redundant_actions');
+function remove_ha_redundant_actions()
 {
     remove_action('woocommerce_product_query', 'custom_search');
+    
 }

9288845add1aeced48521125d79be5fad51799a9
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-11-08 11:34:30 +0000

    fixed product category list

--- wp-content/themes/herbalapothecary-child__futurehealthstore/functions.php
+++ wp-content/themes/herbalapothecary-child__futurehealthstore/functions.php
@@ -2,7 +2,7 @@
 
 function fh_enqueue_assets()
 {
-    wp_enqueue_style('futurehealthstore-style', get_template_directory_uri() . "/style.css", array('herbalapothecary-style'), _S_VERSION);
+    wp_enqueue_style('futurehealthstore-style', get_stylesheet_uri(), array('herbalapothecary-style'), _S_VERSION);
 }
 add_action('wp_enqueue_scripts', 'fh_enqueue_assets');
 
@@ -11,5 +11,7 @@ add_action('init', 'remove_ha_redundant_actions');
 function remove_ha_redundant_actions()
 {
     remove_action('woocommerce_product_query', 'custom_search');
-    
+    remove_filter('woocommerce_catalog_orderby', 'herbalapothecary_add_custom_sorting_options');
 }
+
+

aa91d2062a027f9201f655c79b1aba79b76f2928
Jack Barber <jack@natureslaboratory.co.uk>
2021-11-15 17:30:55 +0000

    Update page-front-page.php

--- wp-content/themes/herbalapothecary-child__futurehealthstore/page-front-page.php
+++ wp-content/themes/herbalapothecary-child__futurehealthstore/page-front-page.php
@@ -17,9 +17,7 @@ get_header();
                 <div class="c-carousel__content">
                     <p class="c-carousel__small-heading">Ethical Goods, Traded Fairly</p>
                     <h2 class="c-carousel__heading">Future Health Store</h2>
-                    <p class="c-carousel__description">
-                        We're a not-for-profit community interest company. We never charge more than RRP.</p>
-                    </p>
+                    <p class="c-carousel__description">We're a not-for-profit community interest company. We never charge more than RRP.</p>
                     <a href="/about-us/" aria-label="About Us"><button class="c-button">Find Out More</button></a>
                 </div>
             </div>

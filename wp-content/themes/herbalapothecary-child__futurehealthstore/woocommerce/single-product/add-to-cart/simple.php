3da58ad8c0f35aebfc1dd88463b27719ff3bea97
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-11-22 14:46:01 +0000

    updated add to cart styles

--- wp-content/themes/herbalapothecary-child__futurehealthstore/woocommerce/single-product/add-to-cart/simple.php
+++ wp-content/themes/herbalapothecary-child__futurehealthstore/woocommerce/single-product/add-to-cart/simple.php
@@ -47,7 +47,9 @@ if ($product->is_in_stock()) : ?>
 		do_action('woocommerce_after_add_to_cart_quantity');
 		?>
 
-		<button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" class="single_add_to_cart_button button alt"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
+		<div class="c-button__cart-wrapper">
+			<button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" class="single_add_to_cart_button button alt"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
+		</div>
 
 		<?php do_action('woocommerce_after_add_to_cart_button'); ?>
 	</form>

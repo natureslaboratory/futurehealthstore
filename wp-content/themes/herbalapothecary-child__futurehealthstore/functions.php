a8772d95f30c9448eb1a75878470999644eb01c6
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-11-23 09:16:54 +0000

    removed excess functions

--- wp-content/themes/herbalapothecary-child__futurehealthstore/functions.php
+++ wp-content/themes/herbalapothecary-child__futurehealthstore/functions.php
@@ -12,6 +12,18 @@ function remove_ha_redundant_actions()
 {
     remove_action('woocommerce_product_query', 'custom_search');
     remove_filter('woocommerce_catalog_orderby', 'herbalapothecary_add_custom_sorting_options');
+    remove_action('wp_dashboard_setup', 'ha_dashboard_widgets');
+    remove_action( 'admin_post_ha_upload_stock', 'ha_upload_handler');
+    remove_action("woocommerce_single_product_summary", "ha_add_organic_flag", 31);
+    remove_action("woocommerce_single_product_summary", "ha_single_product_acf_details", 25);
+    remove_filter('the_content', 'customizing_woocommerce_description');
+    remove_action('woocommerce_created_customer', 'ha_save_register_fields');
+    remove_action('woocommerce_register_form', 'ha_add_register_form_field_top', 20);
+    remove_action('woocommerce_register_form', 'ha_add_register_form_field_bottom', 30);
+    remove_action('woocommerce_register_form_tag', 'ha_enctype_custom_registration_forms');
+    remove_action("woocommerce_register_form_start", "ha_register_top");
+    remove_action("woocommerce_register_form", "ha_register_middle", 25);
+    remove_action("woocommerce_register_form", "ha_register_bottom", 35);
 }
 
 

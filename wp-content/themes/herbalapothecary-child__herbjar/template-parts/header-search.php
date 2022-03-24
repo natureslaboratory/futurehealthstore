8bf598cb5a33e4d8b63bfff2464f9bf339e6b574
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-24 12:06:11 +0000

    Update header-search.php

--- wp-content/themes/herbalapothecary-child__herbjar/template-parts/header-search.php
+++ wp-content/themes/herbalapothecary-child__herbjar/template-parts/header-search.php
@@ -1,9 +1,4 @@
 <form class="c-search <?= array_key_exists("cat", $args) ? "" : "c-search--navigation" ?>">
-    <?php if (array_key_exists("cat", $args)) { ?>
-        <select class="c-search__dropdown search-category">
-            <option value="" selected="selected">All</option>
-        </select>
-    <?php } ?>
     <input class="c-search__box search-text" type="text" placeholder="I'm shopping for...">
     <button type="submit" class="c-button search-submit">Search</button>
 </form>

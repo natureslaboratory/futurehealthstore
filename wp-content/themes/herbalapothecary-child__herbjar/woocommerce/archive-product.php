9a8c30b599ebb6d1703785789dae90173daaddd2
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-24 15:29:51 +0000

    Update archive-product.php

--- wp-content/themes/herbalapothecary-child__herbjar/woocommerce/archive-product.php
+++ wp-content/themes/herbalapothecary-child__herbjar/woocommerce/archive-product.php
@@ -50,11 +50,9 @@ do_action('woocommerce_before_main_content');
 			//get_template_part("template-parts/product-categories", "categories");
 		}
 		
-		<?php if ( is_product_category() || is_product() ) : ?>
-		   <div class="page-header-image generate-page-header category-header">
-		       <div class="grid-container"><h1><?php single_term_title(); ?></h1></div>
-		</div> 
-		<?php endif; ?>
+		<?php if ( is_product_category() ){ ?>
+		   <h1><?php single_term_title(); ?></h1>
+		<?php }
 
 		echo "<div class='c-shop__grid c-shop__grid--category'>";
 

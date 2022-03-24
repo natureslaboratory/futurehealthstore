0fa61bad3e4f9bc5e5af476a89be5c6a68725429
Jack Barber <jack@natureslaboratory.co.uk>
2022-03-24 12:34:17 +0000

    Update footer.php

--- wp-content/themes/herbalapothecary-child__herbjar/footer.php
+++ wp-content/themes/herbalapothecary-child__herbjar/footer.php
@@ -17,42 +17,17 @@
 		<div class="c-footer__herbal">
 			<?php the_custom_logo() ?>
 			<p>Enjoy the regenerative and anti-ageing benefits of healthy and nourishing ingredients, with zero exposure to potentially harmful chemicals.</p>
+			<p><i class="fa fa-phone"></i> 01947 602346 &bull; <a href="mailto:info@herbjar.co.uk">info@herbjar.co.uk</a>
+			<p>Unit 3B, Enterprise Way, Whitby, North Yorkshire, YO22 4NH</p>
 		</div>
 		<div class="c-footer__accreditations">
-			<h2>Accreditations</h2>
-			<p>
-				We are proud to be a real Living Wage employer with ISO9001:2015 quality management
-				systems and organic certification.
-			</p>
-			<div class="c-footer__accreditations-logos">
-				<a rel="noreferrer" href="https://www.livingwage.org.uk/" target="_blank"><img src="/wp-content/uploads/2022/03/living_wage_employer.webp" width="106px" height="84px" alt="Living Wage"></a>
-			</div>
+			
 		</div>
 		<div class="c-footer__info c-footer__column">
-			<h2>Company Info</h2>
-			<a href="tel:+441947 602346">
-				<i class="fa fa-phone"></i> 01947 602346
-			</a>
-			<a href="mailto:info@herbjar.co.uk">
-				<i class="fa fa-envelope"></i> info@herbjar.co.uk
-			</a>
-			<p>
-				<i class="fa fa-map-marker"></i> Unit 3B, Enterprise Way, Whitby, North Yorkshire
-			</p>
+			
 		</div>
 		<div class="c-footer__social c-footer__column">
-<!--
-			<h2>Social</h2>
-			<p>
-				Find us on the following social channels
-			</p>
-			<a href="https://instagram.com/futurehealthstore" class="c-footer__social-link">
-				<i class="fab fa-instagram"></i><span>Instagram</span>
-			</a>
-			<a href="https://www.facebook.com/futurehealthstore/" class="c-footer__social-link">
-				<i class="fab fa-facebook-f"></i><span>Facebook</span>
-			</a>
--->
+
 		</div>
 	</div> <!-- End .c-footer__top -->
 	<div class="c-footer__copywrite">
